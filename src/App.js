cb5bf26f594d7497dfc04336e7fdabd4b1584508
blacksheepevox <tyler.rubino91@gmail.com>
2022-08-04 18:43:45 -0400

    setup navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import GenericLogo from './app/assets/img/generic-logo.png';
 import './App.css';
 
 function App() {
-	return <div className="App">New Gravel App!</div>;
+	return (
+		<div className="App">
+			<Navbar dark color="primary" sticky="top" expand="md">
+				<Container>
+					<NavbarBrand href="/">
+						<img src={GenericLogo} alt="nucamp logo" />
+						<h3>Project Gravel One</h3>
+					</NavbarBrand>
+				</Container>
+			</Navbar>
+			<h1>Any Text Goes Here</h1>
+		</div>
+	);
 }
 
 export default App;
